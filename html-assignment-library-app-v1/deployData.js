7f12db718d1d64de4bcf61a674bb27d7668a5613
adeldl <adellanatasyahutagalung@gmail.com>
2024-10-15 19:31:20 +0700

    assignment-5

---
+++ html-assignment-library-app-v1/deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Adella Natasya'; // TODO: replace this
+
+const studentId = 'FS9471126'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
